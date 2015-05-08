69fd63fb8cd20495fd03dc08badaf5e1724c9e84
sungoin-zbs <sungoin-zbs@sungoindeiMac.local>
2015-05-08 18:39:26 +0800

    Initial Commit

---
+++ MenuDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MenuDemo
+//
+//  Created by sungoin-zbs on 15/5/8.
+//  Copyright (c) 2015年 sungoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MenuDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MenuDemo
+//
+//  Created by sungoin-zbs on 15/5/8.
+//  Copyright (c) 2015年 sungoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
